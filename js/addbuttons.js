bfa49f65b2cd1df715f933d4ea74a2c2a4c9493f
Aryan Prakash <75618742+AryanPrakash@users.noreply.github.com>
2023-10-24 13:14:43 +0530

    Updated addbuttons.js

--- js/addbuttons.js
+++ js/addbuttons.js
@@ -1,13 +1,20 @@
-AFRAME.registerComponent('create-button', {
-    init: function() {
-        var button1 = document.createElement('button')
-        button1.innerHTML = 'RATE-US' //property
-        button1.setAttribute('id', 'rating-button');
-        button1.setAttribute('class', 'btn btn-1');
+AFRAME.registerComponent("create-buttons", {
+  init: function() {
+    // 1. Create the button
+    var button1 = document.createElement("button");
+    button1.innerHTML = "RATE US";
+    button1.setAttribute("id", "rating-button");
+    button1.setAttribute("class", "btn btn-warning");
 
-        var button2 = document.createElement('button') //to create html element within js file
-        button2.innerHTML = 'ORDER NOW' //property to give name to the button
-        button2.setAttribute('id', 'order-button');
-        button2.setAttribute('class', 'btn btn-warning');
-    }
-})
+    // 2. Create the button
+    var button2 = document.createElement("button");
+    button2.innerHTML = "ORDER NOW";
+    button2.setAttribute("id", "order-button");
+    button2.setAttribute("class", "btn btn-warning");
+
+    // 2. Append button elements
+    var buttonDiv = document.getElementById("button-div");
+    buttonDiv.appendChild(button1);
+    buttonDiv.appendChild(button2);
+  }
+});
